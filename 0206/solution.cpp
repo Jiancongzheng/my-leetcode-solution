0dd6d62f9d320f08b667faac838ff9854f7c830c
ShenChingWa <51027080+Jiancongzheng@users.noreply.github.com>
2020-06-16 22:32:04 +0200

    Update solution.cpp

--- 0206/solution.cpp
+++ 0206/solution.cpp
@@ -9,17 +9,28 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (!head or !head->next) return head;
-        ListNode* p, * s;
-        p = new ListNode(head->val);
-        s = head->next;
-        while (s)
-        {
-            ListNode* tmp = new ListNode(s->val);
-            tmp->next = p;
-            p = tmp;
-            s = s->next;
+        ListNode* r;
+        if (head == nullptr or head->next == nullptr) return head;
+        r = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return r;
+    }
+};
+
+class Solution1 {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* r = new ListNode(0);
+        ListNode* p;
+        if (head == nullptr) return head;
+        while (head != nullptr) {
+            p = r->next;
+            r->next = head;
+            head = head->next;
+            r->next->next = p;
+            p = r->next;
         }
-        return p;
+        return r->next;
     }
 };
