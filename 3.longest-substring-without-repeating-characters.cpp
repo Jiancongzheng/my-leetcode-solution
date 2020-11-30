549d921375ccde6e59d49689002c338de715a99a
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-11-30 21:15:09 +0100

    add solution to Q3

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -8,7 +8,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        int ans = 0;
+        if (s.empty()) return ans;
+        int len = s.length();
+        vector<int> dp = vector<int>(len, 1);
+        map<char, int> list;
+        list[s[0]] = 0;
+        ans = 1;
+        for (int i = 1; i < len; ++i) {
+            char ch = s[i];
+            if (list.find(ch) == list.end()) {
+                dp[i] = dp[i-1] + 1;
+            }
+            else {
+                dp[i] = min(i - list[ch], dp[i-1] + 1);
+            }
+            list[ch] = i;
+            ans = max(dp[i], ans);
+        }
+        return ans;
     }
 };
 // @lc code=end
