4c26cf8d61c63e3156ac24dd462c2467b4846474
Jiancong Zheng <jiancong.zheng@outlook.com>
2020-12-10 22:32:34 +0100

    Add solution to Q46

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -6,6 +6,34 @@
 
 // @lc code=start
 class Solution {
+private:
+    void dfs(vector<vector<int>>& ans, vector<int>& permutation, 
+             vector<bool>& visited, vector<int>& nums) {
+        if (permutation.size() == nums.size()) {
+            ans.emplace_back(permutation);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            permutation.push_back(nums[i]);
+            dfs(ans, permutation, visited, nums);
+            permutation.pop_back();
+            visited[i] = false;
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> permutation;
+        vector<bool> visited(nums.size(), false);
+        dfs(ans, permutation, visited, nums);
+        return ans;
+    }
+};
+
+class Solution1 {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans{{}};
