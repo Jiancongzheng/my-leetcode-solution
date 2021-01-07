624087a93d54ba9bc778993965b8b873bc2ee748
Jiancong Zheng <jiancong0204@outlook.com>
2021-01-07 23:39:55 +0100

    Add solution to Q300

---
+++ 300.longest-increasing-subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=300 lang=cpp
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = nums.size();
+        int ans = 1;
+        vector<int> dp(len, 1);
+        for (int i = 1; i < len; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    ans = max(ans, dp[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
