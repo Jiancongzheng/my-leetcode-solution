1a60e5dd9c0f4a6851f037665aad41a083f89c84
Jiancong Zheng <jiancong0204@outlook.com>
2021-01-08 00:49:30 +0100

    Add comments

--- 300.longest-increasing-subsequence.cpp
+++ 300.longest-increasing-subsequence.cpp
@@ -13,16 +13,23 @@ public:
         vector<int> dp;
         dp.emplace_back(nums.front());
         for (int& num: nums) {
+            // Binary search
             int l = 0;
             int r = dp.size() - 1;
+
+            // Find the first element in dp, which is greater and equal to num.
             while (l < r) {
                 int m = (l + r) / 2;
                 if (dp[m] < num) l = m + 1;
                 else r = m;
             }
+
+            // if such element can not be found (All the elements in dp are smaller than num)
             if (dp[l] < num) {
                 dp.emplace_back(num);
             }
+
+            // Otherwise
             else dp[l] = num;
         }
         return dp.size();
