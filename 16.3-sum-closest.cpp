4a51f98c14734201d26adeffb07363e010c009cc
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-13 15:22:43 +0100

    Update solution

--- 16.3-sum-closest.cpp
+++ 16.3-sum-closest.cpp
@@ -18,13 +18,7 @@ public:
             while (l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
                 if (sum == target) return sum;
-                
                 if (abs(ans - target) > abs(sum - target)) ans = sum;
-<<<<<<< HEAD
-                
-=======
-
->>>>>>> c23fdb5ddedc8d90ea3119c2eecd5b7533dc0eaa
                 if (sum < target) ++l;
                 else --r;
             }
