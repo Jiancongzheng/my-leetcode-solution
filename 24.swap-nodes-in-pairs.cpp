61dd063a28bd39568529fdc2bbb934546a56f4a4
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-15 15:03:52 +0100

    Add solution to Q24

--- 24.swap-nodes-in-pairs.cpp
+++ 24.swap-nodes-in-pairs.cpp
@@ -15,12 +15,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution1 {
 public:
     ListNode* swapPairs(ListNode* head) {
         auto* h = head;
         while (h != nullptr and h->next != nullptr) {
             swap(h->val, h->next->val);
+            h = h->next->next;
+        }
+        return head;
+    }
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr or head->next == nullptr) return head;
+        auto* h = head->next;
+        head->next = h->next;
+        h->next = head;
+        head = h;
+        h = head->next;
+
+        while (h->next != nullptr and h->next->next != nullptr) {
+            auto *tmp1 = h->next;
+            auto *tmp2 = h->next->next;
+            h->next = tmp2;
+            tmp1->next = tmp2->next;
+            tmp2->next = tmp1; 
             h = h->next->next;
         }
         return head;
