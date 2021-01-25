d9ea5b43cf352796e48449c643b7022854d00ea8
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-25 21:17:02 +0100

    Add solution to Q55

---
+++ 55.jump-game.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=55 lang=cpp
+ *
+ * [55] Jump Game
+ */
+
+// @lc code=start
+
+// Dynamic programming
+class Solution1 {
+public:
+    bool canJump(vector<int>& nums) {
+        int len = nums.size();
+        vector<bool> dp(len, false);
+        dp.back() = true;
+        for (int i = len - 2; i >= 0; --i) {
+            for (int j = i + 1; j < len and j <= i + nums[i]; ++j) {
+                if (dp[j]) {
+                    dp[i] = true;
+                    break;
+                } 
+            }
+        }
+        return dp.front();
+    }
+};
+
+// Greedy
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int pos = nums.size() - 1;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (i + nums[i] >= pos) pos = i;
+        }
+        return pos == 0;
+    }
+};
+// @lc code=end
+
