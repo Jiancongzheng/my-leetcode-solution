38461248788b356edd06807876a248f21083470f
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-26 19:54:16 +0100

    Update solution

--- 62.unique-paths.cpp
+++ 62.unique-paths.cpp
@@ -9,9 +9,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<int> dp(n, 1);
-        for (int i = 1; i < m; ++i) {
-            for (int j = n - 2; j >= 0; --j) {
-                dp[j] = dp[j] + dp[j + 1];
+        for (int j = 1; j < m; ++j) {
+            for (int i = n - 2; i >= 0; --i) {
+                dp[i] = dp[i] + dp[i + 1];
             }
         } 
         return dp.front();
