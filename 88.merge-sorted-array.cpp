6bdcf5692fe8e4e06fc1ea6429aaf5e853d7ccd0
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-30 19:25:44 +0100

    Add solution to Q88

---
+++ 88.merge-sorted-array.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=88 lang=cpp
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0) return;
+        int len = m + n;
+        int j = 0;
+        int i = 0;
+        for (; i < m and j < n; ++i) {
+            if (nums1[i] > nums2[j]) {
+                swap(nums1[i], nums2[j]);
+                ++j;
+            }
+        }
+        int tmpn = j;
+        for (i = m; i < len and j < n; ++i, ++j) {
+            nums1[i] = nums2[j];
+        }
+        merge(nums1, i, nums2, tmpn);
+    }
+};
+// @lc code=end
+
