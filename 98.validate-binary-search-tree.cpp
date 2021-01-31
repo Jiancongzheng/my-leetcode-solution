2326f679d0c1d42bd4514426cf5ca2b2d15c006a
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-01-31 21:44:42 +0100

    Add solution to Q98

--- 98.validate-binary-search-tree.cpp
+++ 98.validate-binary-search-tree.cpp
@@ -16,7 +16,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+// Solution 1: 递归
+// Solution 2: 中序遍历
+
+class SolutionRecursion {
 private:
     bool helper(TreeNode* root, long maxVal, long minVal) {
         if (root == nullptr) return true;
@@ -26,6 +30,27 @@ private:
 public:
     bool isValidBST(TreeNode* root) {
         return helper(root, LONG_MAX, LONG_MIN);
+    }
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* n = root;
+        long prev = LONG_MIN;
+        while (n != nullptr or !stk.empty()) {
+            while (n != nullptr) {
+                stk.push(n);
+                n = n->left;
+            }
+            n = stk.top();
+            if (n->val <= prev) return false;
+            prev = n->val;
+            stk.pop();
+            n = n->right;
+        }
+        return true;
     }
 };
 // @lc code=end
