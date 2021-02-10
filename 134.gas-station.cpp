9b24caebec0eab5546df884050d580956c4328c7
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-02-10 19:53:17 +0100

    Update solution

--- 134.gas-station.cpp
+++ 134.gas-station.cpp
@@ -9,18 +9,18 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int len = gas.size();
-        int sum = 0;
-        int tank = 0;
-        int ans = 0;
-        for (int i = 0; i < len; ++i) {
-            tank += gas[i] - cost[i];
-            sum  += gas[i] - cost[i];
-            if (sum < 0) {
-                ans = i + 1;
-                sum = 0;
+        for (int i = 0; i < len;) {
+            int car = 0;
+            int cnt = 0;
+            while (cnt < len) {
+                int curr = (i + cnt) % len;
+                car += gas[curr] - cost[curr];
+                if (car < 0) break;
+                ++cnt;
             }
+            if (cnt == len) return i;
+            i = i + cnt + 1;
         }
-        if (tank >= 0) return ans;
         return -1;
     }
 };
