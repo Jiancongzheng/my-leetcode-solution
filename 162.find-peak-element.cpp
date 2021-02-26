2065b6f060590e1de2cef94ee95240b427832ef3
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-02-26 21:58:39 +0100

    Update solution

--- 162.find-peak-element.cpp
+++ 162.find-peak-element.cpp
@@ -8,10 +8,13 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            if (nums[i + 1] < nums[i]) return i;
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[m + 1]) l = m + 1;
+            else r = m; 
         }
-        return nums.size() - 1;
+        return l;
     }
 };
 // @lc code=end
