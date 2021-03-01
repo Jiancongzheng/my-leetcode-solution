c651c5b523621709afc9e3123407146bb4189816
Jiancong Zheng <jiancong.zheng@outlook.com>
2021-03-01 18:55:36 +0100

    Update solution

--- 575.distribute-candies.cpp
+++ 575.distribute-candies.cpp
@@ -5,13 +5,29 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
     int distributeCandies(vector<int>& candyType) {
         unordered_set<int> s(candyType.begin(), candyType.end());
         int cntType = s.size();
         int cntCndy = candyType.size() / 2;
         return min(cntType, cntCndy);
+    }
+};
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        sort(candyType.begin(), candyType.end());
+        int pre = candyType.front();
+        int cnt = 1;
+        for (int& c : candyType) {
+            if (c == pre) continue;
+            pre = c;
+            ++cnt;
+        }
+        if (cnt < candyType.size() / 2) return cnt;
+        return candyType.size() / 2;
     }
 };
 // @lc code=end
